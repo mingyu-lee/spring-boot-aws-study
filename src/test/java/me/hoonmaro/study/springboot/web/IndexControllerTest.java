e79b7c6d241457acda919b5fb3b63284cf883893
Mingyu Lee <leemingyu05@gmail.com>
2020-06-25 22:12:13 +0900

    mustache index 페이지 생성 및 테스트 구현

---
+++ src/test/java/me/hoonmaro/study/springboot/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package me.hoonmaro.study.springboot.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @DisplayName("메인페이지 로딩")
+    @Test
+    void index_page() {
+        String body = this.testRestTemplate.getForObject("/", String.class);
+
+        assertThat(body).contains("<h1>스프링 부트 AWS 공부</h1>");
+    }
+
+}
