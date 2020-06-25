5816da157b6dd56dbd242a2f5d522c1f881892b1
Mingyu Lee <leemingyu05@gmail.com>
2020-06-25 21:27:22 +0900

    JpsAuditing 사용

--- src/main/java/me/hoonmaro/study/springboot/domain/posts/Posts.java
+++ src/main/java/me/hoonmaro/study/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package me.hoonmaro.study.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.hoonmaro.study.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
