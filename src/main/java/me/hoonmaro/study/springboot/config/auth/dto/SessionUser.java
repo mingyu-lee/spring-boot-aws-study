dfc9bad6a5b38c2fd17d664d9e5475f9950e64d9
Mingyu Lee <leemingyu05@gmail.com>
2020-06-28 01:04:50 +0900

    Spring Security OAuth2 Client 구현하여 구글 로그인 연동

---
+++ src/main/java/me/hoonmaro/study/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package me.hoonmaro.study.springboot.config.auth.dto;
+
+import lombok.Getter;
+import me.hoonmaro.study.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/me/hoonmaro/study/springboot/web/IndexController.java
+++ src/main/java/me/hoonmaro/study/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.hoonmaro.study.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.hoonmaro.study.springboot.config.auth.dto.SessionUser;
 import me.hoonmaro.study.springboot.service.posts.PostsService;
 import me.hoonmaro.study.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
